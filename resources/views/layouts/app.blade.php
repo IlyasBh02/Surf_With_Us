f56132ce34ae53b34ab5ea260c3f3213aa1bf9b8
IlyasBh02 <ilyasbahsi9@gmail.com>
2026-04-26 17:40:58 +0100

    fix the route of the reservation of a cours in the surfer session

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,7 +53,7 @@
                                 <i class="fa-solid fa-graduation-cap mr-1"></i> Courses
                             </a>
                             @if(Auth::check() && Auth::user()->role === 'surfeur')
-                                <a href="{{ route('reservations.index') }}" class="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
+                                <a href="{{ route('surfer.reservations') }}" class="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
                                     <i class="fa-solid fa-calendar-check mr-1"></i> My Reservations
                                 </a>
                             @endif
@@ -121,7 +121,7 @@
                         <i class="fa-solid fa-graduation-cap mr-1"></i> Courses
                     </a>
                     @if(Auth::check() && Auth::user()->role === 'surfeur')
-                        <a href="{{ route('reservations.index') }}" class="border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">
+                        <a href="{{ route('surfer.reservations') }}" class="border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">
                             <i class="fa-solid fa-calendar-check mr-1"></i> My Reservations
                         </a>
                     @endif
